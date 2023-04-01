0f3d8a964b8e68f9f0f676902c33fd26ced1afd2
Dorje Gilfillan <dorjegilfillan@gmail.com>
2023-04-01 21:59:04 +0200

    fixed benchmarks by using std::hint::black_box

--- benches/bit_array.rs
+++ benches/bit_array.rs
@@ -1,33 +1,28 @@
 use bit_array::BitArray;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
 #[allow(clippy::missing_panics_doc)]
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("construct bit array", |b| {
         b.iter(|| {
-            let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = BitArray::new();
-                sum += array.len();
+            for _ in 0..1000 {
+                black_box(BitArray::new());
             }
-            black_box(sum);
         });
     });
     c.bench_function("construct bool array", |b| {
         b.iter(|| {
-            let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array: [bool; 64] = [false; 64];
-                sum += array.len();
+            for _ in 0..1000 {
+                black_box([false; 64]);
             }
-            black_box(sum);
         });
     });
     c.bench_function("sum bit array", |b| {
         b.iter(|| {
             let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = BitArray::from(u64::MAX);
+            for _ in 0..1000 {
+                let array = black_box(BitArray::from(u64::MAX));
                 sum += array.num_active();
             }
             black_box(sum);
@@ -36,8 +31,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sum iter bit array", |b| {
         b.iter(|| {
             let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = BitArray::from(u64::MAX);
+            for _ in 0..1000 {
+                let array = black_box(BitArray::from(u64::MAX));
                 sum += array.into_iter().map(u64::from).sum::<u64>();
             }
             black_box(sum);
@@ -46,8 +41,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sum iter bool array", |b| {
         b.iter(|| {
             let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = [true; 64];
+            for _ in 0..1000 {
+                let array = black_box([true; 64]);
                 sum += array.into_iter().map(u64::from).sum::<u64>();
             }
             black_box(sum);
@@ -56,8 +51,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sum loop bit array", |b| {
         b.iter(|| {
             let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = BitArray::from(u64::MAX);
+            for _ in 0..1000 {
+                let array = black_box(BitArray::from(u64::MAX));
                 for i in 0..64 {
                     sum += usize::from(array.get_unchecked(i));
                 }
@@ -68,8 +63,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sum loop bool array", |b| {
         b.iter(|| {
             let mut sum = 0;
-            for _ in 0..black_box(1000) {
-                let array = [true; 64];
+            for _ in 0..1000 {
+                let array = black_box([true; 64]);
                 for i in 0..64 {
                     sum += usize::from(*array.get(i).unwrap());
                 }
